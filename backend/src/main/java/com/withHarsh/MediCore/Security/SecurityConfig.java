8f7b7b39ec4d960311e894554879ea300663fc60
HarshVathare <harsh1234vathare@gmail.com>
2026-05-07 21:42:31 +0530

    my backend envfile updated

--- backend/src/main/java/com/withHarsh/MediCore/Security/SecurityConfig.java
+++ backend/src/main/java/com/withHarsh/MediCore/Security/SecurityConfig.java
@@ -102,4 +102,17 @@ public CorsConfigurationSource corsConfigurationSource() {
 }
 
 
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("https://yourapp.vercel.app")
+//                        .allowedMethods("*");
+//            }
+//        };
+//    }
+
+
 }
